34b1fd2cbed9533dd8f413f0d23d98f6682c154f
A Hoerman <awhoerman@me.com>
2020-03-08 17:51:03 -0400

    added arrays together based on choices

--- password.js
+++ password.js
@@ -1,28 +1,39 @@
-class PasswordObj {
-    constructor(length, characters){
+class PasswordGenObj {
+    constructor(length, characters) {
         this.length = length;
         this.characters = characters;
     }
     generatePassword() {
-        const uppercase = ["A", "B", "C"];
-        const lowercase = ["a","b","c"];
+        const uppercase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+        const lowercase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+        const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0]
+        const specialChar = ["!", "@", "#", "$", "&", "*", "?"]
 
-        const newPasswordArray = [];
+        let newPasswordArray = [];
 
-        if(this.length > 8 && this.length < 128) {
-            console.log("Good Choice!")
-            // if (this.characters.includes("Uppercase Letters")){
-            //     newPasswordArray += [...uppercase];
-            // }
-            // if (this.characters.includes("Lowercase Letters")){
-            //     newPasswordArray += [...lowercase];
-            // }
-            // console.log(newPasswordArray);
+        if (this.length > 8 && this.length < 128) {
+            if ((this.characters).includes("Uppercase Letters")) {
+                newPasswordArray = [...newPasswordArray, ...uppercase];
+            }
+            if ((this.characters).includes("Lowercase Letters")) {
+                newPasswordArray = [...newPasswordArray, ...lowercase];
+            }
+            if ((this.characters).includes("Numbers")) {
+                newPasswordArray = [...newPasswordArray, ...numbers];
+            }
+            if ((this.characters).includes("Special Characters")) {
+                newPasswordArray = [...newPasswordArray, ...specialChar];
+            } else {
+                console.log("Please pick atleast one set of characters!")
+                // passwordPrompts();
+            }
+            console.log(newPasswordArray);
+            console.log(newPasswordArray[25]);
         } else {
             console.log("Please pick a length between 8 and 128!");
-            passwordPrompts();
+            // passwordPrompts();
         }
     }
 }
 
-module.exports = PasswordObj;
+module.exports = PasswordGenObj;
